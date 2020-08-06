b8e40cdc82724c1154d0f6a3a2a917b3b1d46a55
Ameya Ghag <ameyaughag@gmail.com>
2020-08-05 23:59:38 -0400

    Delete ANLY 530 Final Code AG.R

--- ANLY 530 Final Code AG.R
+++
@@ -1,9 +0,0 @@
-
-
-#Set working directory
-getwd()
-setwd("/Users/ameyaghag/Documents/Harrisburg University/ANLY 530/Final Project")
-
-#reading the excel file into a dataset
-
-emp_absent = read.xlsx(file = "Absenteeism_at_work.xls", header = T, sheetIndex = 1)

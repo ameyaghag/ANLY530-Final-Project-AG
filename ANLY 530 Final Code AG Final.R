5e5af1e364ea64e33798d56dc5d8c6d03f156382
Ameya Ghag <ameyaughag@gmail.com>
2020-08-05 23:58:06 -0400

    Update ANLY 530 Final Code AG Final.R

--- ANLY 530 Final Code AG Final.R
+++ ANLY 530 Final Code AG Final.R
@@ -11,6 +11,7 @@ setwd("/Users/ameyaghag/Documents/Harrisburg University/ANLY 530/Final Project/A
 employee <- read.csv("/Users/ameyaghag/Documents/Harrisburg University/ANLY 530/Final Project/ANLY530-Final-Project-AG/Absenteeism_at_work_train.csv")
 summary(employee)
 na.omit(employee) #removing any NA values present in this dataset
+View(employee)
 
 dim(employee)
 #factorizing each employee since they are unique IDs for each employee
@@ -18,6 +19,18 @@ employee$ID = as.factor(as.character(employee$ID))
 
 range(employee$Month.of.absence)
 employee$Month.of.absence = as.factor(as.character(employee$Month.of.absence))
+
+employee$Reason.for.absence = as.factor(as.character(employee$Reason.for.absence))
+
+employee$Day.of.the.week = as.factor(as.character(employee$Day.of.the.week))
+employee$Social.drinker = as.factor(as.character(employee$Social.drinker))
+employee$Social.smoker = as.factor(as.character(employee$Social.smoker))
+employee$Seasons = as.factor(as.character(employee$Seasons))
+employee$Disciplinary.failure = as.factor(as.character(employee$Disciplinary.failure))
+employee$Education = as.factor(as.character(employee$Education))
+employee$Son = as.factor(as.character(employee$Son))
+employee$Pet = as.factor(as.character(employee$Pet))
+
 
 
 

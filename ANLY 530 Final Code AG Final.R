3d352db7374265a2986e530b0dfd7fb03e347236
Ameya Ghag <ameyaughag@gmail.com>
2020-08-05 23:53:00 -0400

    Update ANLY 530 Final Code AG Final.R

--- ANLY 530 Final Code AG Final.R
+++ ANLY 530 Final Code AG Final.R
@@ -8,6 +8,16 @@ setwd("/Users/ameyaghag/Documents/Harrisburg University/ANLY 530/Final Project/A
 
 #reading the excel file into a dataset
 
-data <- read.csv("/Users/ameyaghag/Documents/Harrisburg University/ANLY 530/Final Project/ANLY530-Final-Project-AG/Absenteeism_at_work_train.csv")
-summary(data)
+employee <- read.csv("/Users/ameyaghag/Documents/Harrisburg University/ANLY 530/Final Project/ANLY530-Final-Project-AG/Absenteeism_at_work_train.csv")
+summary(employee)
+na.omit(employee) #removing any NA values present in this dataset
+
+dim(employee)
+#factorizing each employee since they are unique IDs for each employee
+employee$ID = as.factor(as.character(employee$ID))
+
+range(employee$Month.of.absence)
+employee$Month.of.absence = as.factor(as.character(employee$Month.of.absence))
+
+
 

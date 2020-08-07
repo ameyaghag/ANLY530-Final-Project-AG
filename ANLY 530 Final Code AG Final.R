cb1a82f88bf168b6eec67f890b1fef04b2a3f9c9
Ameya Ghag <ameyaughag@gmail.com>
2020-08-07 02:05:54 -0400

    Update ANLY 530 Final Code AG Final.R

--- ANLY 530 Final Code AG Final.R
+++ ANLY 530 Final Code AG Final.R
@@ -12,69 +12,77 @@ setwd("/Users/ameyaghag/Documents/Harrisburg University/ANLY 530/Final Project/A
 employee <- read.csv("/Users/ameyaghag/Documents/Harrisburg University/ANLY 530/Final Project/
                      ANLY530-Final-Project-AG/Absenteeism_at_work_train.csv")
 summary(employee)
-na.omit(employee) #removing any NA values present in this dataset
-View(employee)
-
 dim(employee)
-#factorizing each employee since they are unique IDs for each employee
-employee$ID = as.factor(as.character(employee$ID))
-
-range(employee$Month.of.absence)
-
-#cleaning data and making factors of many categorical variables
-
-employee$Month.of.absence = as.factor(as.character(employee$Month.of.absence))
-employee$Reason.for.absence = as.factor(as.character(employee$Reason.for.absence))
-employee$Day.of.the.week = as.factor(as.character(employee$Day.of.the.week))
-employee$Social.drinker = as.factor(as.character(employee$Social.drinker))
-employee$Social.smoker = as.factor(as.character(employee$Social.smoker))
-employee$Seasons = as.factor(as.character(employee$Seasons))
-employee$Disciplinary.failure = as.factor(as.character(employee$Disciplinary.failure))
-employee$Education = as.factor(as.character(employee$Education))
-employee$Son = as.factor(as.character(employee$Son))
-employee$Pet = as.factor(as.character(employee$Pet))
-
-str(employee)
-
-#missing value analysis
+#missing value analysis - showed we have 3 cases of NA
 
 as.matrix(colSums(is.na(employee)))
 
-#we can see one missing value in Hit.target and 2 in Weight
+#removing any NA values present in this dataset
+df<-na.omit(employee) 
 
-df<-na.omit(employee)
+as.matrix(colSums(is.na(df)))
+
 dim(df)
 
-#now we have 663 rows after removing the 3 missing cases
+#cleaning data and making factors of many categorical variables
+df$ID = as.factor(as.character(df$ID))
+df$Month.of.absence = as.factor(as.character(df$Month.of.absence))
+df$Reason.for.absence = as.factor(as.character(df$Reason.for.absence))
+df$Day.of.the.week = as.factor(as.character(df$Day.of.the.week))
+df$Social.drinker = as.factor(as.character(df$Social.drinker))
+df$Social.smoker = as.factor(as.character(df$Social.smoker))
+df$Seasons = as.factor(as.character(df$Seasons))
+df$Disciplinary.failure = as.factor(as.character(df$Disciplinary.failure))
+df$Education = as.factor(as.character(df$Education))
+df$Son = as.factor(as.character(df$Son))
+df$Pet = as.factor(as.character(df$Pet))
 
-sapply(df,function(x){sum(is.na(x))})
+str(df)
 
-## Box plot of Absenteeism time in hours with Reason for absence. To verify the outliers in each reason codes.
+employee_f<-df
 
-bar1 = ggplot(data = df, aes(x = ID)) + geom_bar() + ggtitle("Count of ID") + theme_bw()
+#EXPLORATORY ANALYSIS
 
-bar2 = ggplot(data = df, aes(x = Reason.for.absence)) + geom_bar() + 
-  ggtitle("Count of Reason for absence") + theme_bw()
+ggplot(employee_f,
+       aes_string(y=employee_f$Absenteeism.time.in.hours,x=as.factor(employee_f$Reason.for.absence))) +
+  geom_boxplot() + 
+  xlab('Reason Codes for Absence') +
+  ylab('Absenteeism Time in Hours')
 
-bar3 = ggplot(data = df, aes(x = Month.of.absence)) + geom_bar() + ggtitle("Count of Month") + theme_bw()
-bar3
+#In the plot below, we look for a distribution of absenteeism time in hours
+#we notice that there is a very big skew to the right - likely due to outliers
 
-bar4 = ggplot(data = df, aes(x = Disciplinary.failure)) + geom_bar() + 
-  ggtitle("Count of Disciplinary failure") + theme_bw()
+hist(employee_f$Absenteeism.time.in.hours, breaks = 30,
+     xlab = 'Absenteeism Time in hours', main = " Absenteeism Time in Hours - Distribution", col = "blue")
 
-#Check the distribution of numerical data using histogram
-numeric_index = sapply(df, is.numeric)
-numeric_data = df[,numeric_index]
-
-(hist1 = ggplot(data = numeric_data, aes(x =Transportation.expense)) + 
-  ggtitle("Transportation.expense") + geom_histogram(bins = 25))
+#now plotting some histograms below to see how various categorical factors affect absenteeism
+library(gridExtra)
+library(grid)
 
 
-(hist2 = ggplot(data = numeric_data, aes(x =Height)) + 
-  ggtitle("Distribution of Height") + geom_histogram(bins = 25))
 
-(hist3 = ggplot(data = numeric_data, aes(x =Body.mass.index)) + 
-  ggtitle("Distribution of Body.mass.index") + geom_histogram(bins = 25))
+smoking <- ggplot(employee_f, aes(x =  Social.smoker, fill =  Social.smoker))+
+                                   labs(x = 'Is a Social Smoker (1) or Not(0) ?', title = " Social Smoking Histogram") + geom_bar()
 
-(hist4 = ggplot(data = numeric_data, aes(x =Absenteeism.time.in.hours)) + 
-  ggtitle("Distribution of Absenteeism.time.in.hours") + geom_histogram(bins = 25))
+#very few smokers in this dataset, so not likely to have a significant bearing
+
+drink <- ggplot(employee_f, aes(x =  Social.drinker, fill =  Social.drinker)) + geom_bar()+
+  labs(x = 'Is a Social Drinker (1) or Not(0) ?', title = " Social Drinking Histogram")
+
+
+#both are almost close. not a big surprise since many employees might enjoy a social drink or two
+#this is a fairly balanced variable
+
+kids <- ggplot(employee_f, aes(x = Son, fill = Son)) + geom_bar() +
+  labs(x = 'Number of Children', title = "Distribution of # of Children")
+
+#most employees have 1-2 or no kids at all
+
+seasons <- ggplot(employee_f, aes(x =Seasons,fill = Seasons)) + geom_bar() + 
+  labs(x = 'Season Code', title = "Count of Absenteeism Recorded by Season Code")
+
+#even the seasons histogram is fairly balanced between 1 and 3
+
+grid.arrange(smoking,drink,kids,seasons, nrow = 2)
+
+#exploratory analysis doesnt give us a significant idea about reasons for absenteeism
